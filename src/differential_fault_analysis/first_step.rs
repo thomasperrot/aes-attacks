f0932b65a9fe711971ee27b889021455283f7d25
Thomas PERROT <thomas.perrot1@gmail.com>
2025-03-20 18:39:23 +0100

    first commit dfa

--- src/differential_fault_analysis/first_step.rs
+++ src/differential_fault_analysis/first_step.rs
@@ -1,19 +1,71 @@
 use crate::utils::constants::REVERSED_S_BOX;
-use crate::utils::multiply::MULTIPLICATION_BY_2;
-use crate::utils::types::State;
+use crate::utils::multiply::{MULTIPLICATION_BY_2, MULTIPLICATION_BY_3};
+use crate::utils::types::{Block, State};
+use arr_macro::arr;
+
 
 pub fn get_all_possible_keys(normal_state: State, faulty_state: State) {
-
+    let d1_equations = compute_first_column(normal_state, faulty_state);
+    let d2_equations = compute_second_column(normal_state, faulty_state);
+    let d3_equations = compute_third_column(normal_state, faulty_state);
+    let d4_equations = compute_fourth_column(normal_state, faulty_state);
 }
 
-// fn compute_first_column(normal_state: State, faulty_state: State) {
-//     let mut potential_d_1: [Vec<u8>; 0x100] = std::iter::repeat(vec![]).take(10).collect::<Vec<_>>();
-//     for d in 1..0x100 {
-//         for k in 0..=0xff{
-//             let diff = REVERSED_S_BOX[normal_state[0][0] ^ k]  ^ REVERSED_S_BOX[faulty_state[0][0] ^ k];
-//             if MULTIPLICATION_BY_2[d] = diff {
-//                 potential_d_1[d].push(k);
-//             }
-//         }
-//     }
-// }
+fn get_keys(equations: [[u8;0x100];16]) -> Vec<Block> {
+    let keys: Vec<Block> = Vec::new();
+    equations.
+}
+
+/// TODO: skip irrelevant d when previous iterations did not find it
+
+fn compute_first_column(normal_state: State, faulty_state: State) -> Vec<[Vec<u8>; 4]>{
+    // x1
+    let mut potential_d_1: [Vec<u8>; 0x100] = arr![Vec::new(); 0x100];
+    let mut potential_d_2: [Vec<u8>; 0x100] = arr![Vec::new(); 0x100];
+    let mut potential_d_3: [Vec<u8>; 0x100] = arr![Vec::new(); 0x100];
+    let mut potential_d_4: [Vec<u8>; 0x100] = arr![Vec::new(); 0x100];
+    let mut equations = Vec::new();
+    for d in 1..0x100 {
+        for k in 0..=0xff{
+            let diff = REVERSED_S_BOX[normal_state[0][0] ^ k]  ^ REVERSED_S_BOX[faulty_state[0][0] ^ k];
+            if MULTIPLICATION_BY_2[d] == diff {
+                potential_d_1[d].push(k);
+            }
+        }
+    }
+    // x14
+    for d in 1..0x100 {
+        for k in 0..=0xff{
+            let diff = REVERSED_S_BOX[normal_state[1][3] ^ k]  ^ REVERSED_S_BOX[faulty_state[1][3] ^ k];
+            if d == diff {
+                potential_d_2[d].push(k);
+            }
+        }
+    }
+    // x11
+    for d in 1..0x100 {
+        for k in 0..=0xff{
+            let diff = REVERSED_S_BOX[normal_state[2][2] ^ k]  ^ REVERSED_S_BOX[faulty_state[2][2] ^ k];
+            if d == diff {
+                potential_d_3[d].push(k);
+            }
+        }
+    }
+    // x8
+    for d in 1..0x100 {
+        for k in 0..=0xff{
+            let diff = REVERSED_S_BOX[normal_state[3][1] ^ k]  ^ REVERSED_S_BOX[faulty_state[3][1] ^ k];
+            if MULTIPLICATION_BY_3[d] == diff {
+                potential_d_4[d].push(k);
+            }
+        }
+    }
+
+    let mut equations = Vec::new();
+    for i in 0..0x100 {
+        if !potential_d_1[i].is_empty() && !potential_d_2[i].is_empty() && !potential_d_3[i].is_empty() && !potential_d_4.is_empty() {
+            equations.push([potential_d_1[i], potential_d_2[i], &potential_d_3[i], &potential_d_4[i]]);
+        }
+    }
+    equations
+}
